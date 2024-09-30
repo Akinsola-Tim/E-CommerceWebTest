3bbbcad138f9fa0cf135d14e657b2224eebf2e80
Timothy Akinsola <akinsola42@gmail.com>
2024-09-30 22:16:10 +0100

    Reports by mocha added

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+    reporter: 'mochawesome',
+    reporterOptions: {
+    reportDir: 'cypress/reports/mochawesome',
+    overwrite: false,
+    html: false,
+    json: true,
+  },
+
   e2e: {
     
     //experimentalSessionAndOrigin: true,
